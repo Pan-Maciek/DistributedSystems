8594efdaea3e7ca3a573537b5258daa703ecf511
Pan-Maciek <koziejka.com@gmail.com>
2021-03-02 17:41:11 +0100

    setup

---
+++ zad1/Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+const int port = 3000;
+
+Console.Write("Enter username: ");
+var username = Console.ReadLine()!;
+
+using var tcpClient = new TcpClient();
+tcpClient.Connect(IPAddress.Loopback, port);
+
+Console.WriteLine("Connected with server");
---
+++ zad1/Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+const int port = 3000;
+var serverEndPoint = new IPEndPoint(IPAddress.Loopback, port);
+
+var tcpListener = new TcpListener(serverEndPoint);
+tcpListener.Start();
+
+Console.WriteLine($"Server listening at {serverEndPoint}");
+while (true)
+{
+    var tcpClient = tcpListener.AcceptTcpClient();
+    Console.WriteLine("Accepted tcp client");
+}
