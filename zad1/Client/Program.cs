d2e4a24280019aa278f14905dd51fa89a08729f3
Pan-Maciek <koziejka.com@gmail.com>
2021-03-06 12:15:57 +0100

    added UDP multicast

--- zad1/Client/Program.cs
+++ zad1/Client/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using static System.Text.Encoding;
 
 const int port = 3000;
+const int multiCastPort = 4000;
 var serverEndPoint = new IPEndPoint(IPAddress.Loopback, port);
+var localMultiCastEndPoint = new IPEndPoint(IPAddress.Any, multiCastPort);
+var multiCastGroupAddress = IPAddress.Parse("225.0.0.1");
+var remoteMultiCastEndPoint = new IPEndPoint(multiCastGroupAddress, multiCastPort);
 
 Console.Write("Enter username: ");
 var username = Console.ReadLine()!;
@@ -19,6 +23,12 @@ var localPort = ((IPEndPoint) tcpSocket.LocalEndPoint!).Port;
 var udpEndPoint = new IPEndPoint(IPAddress.Loopback, localPort);
 
 udpSocket.Bind(udpEndPoint);
+
+var multiCastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+multiCastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+multiCastSocket.Bind(localMultiCastEndPoint);
+var multiCastOption = new MulticastOption(multiCastGroupAddress, IPAddress.Any);
+multiCastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, multiCastOption);
 
 Console.WriteLine("Connected with server");
 
@@ -39,6 +49,14 @@ void HandleUdpMessage() {
     }
 }
 
+void HandleUdpMulticastMessage() {
+    var buffer = new byte[1024];
+    while (true) {
+        var len = multiCastSocket!.Receive(buffer);
+        Console.WriteLine(UTF8.GetString(buffer, 0, len));
+    }
+}
+
 void HandleTcpMessage() {
     while (true) {
         var message = reader!.ReadString();
@@ -48,13 +66,17 @@ void HandleTcpMessage() {
 
 Task.Factory.StartNew(HandleTcpMessage);
 Task.Factory.StartNew(HandleUdpMessage);
+Task.Factory.StartNew(HandleUdpMulticastMessage);
 
 while (true) {
     Console.Write($"{username}> ");
     var message = Console.ReadLine()!;
     if (message == "U") { // send UDP UniCast message
-        var payload = UTF8.GetBytes("This message is send via UDP datagram UniCast");
+        var payload = UTF8.GetBytes("This message is sent via UDP datagram UniCast");
         udpSocket.SendTo(payload, serverEndPoint);
+    } else if (message == "M") {
+        var payload = UTF8.GetBytes("This message is sent via UDP datagram MultiCast");
+        multiCastSocket.SendTo(payload, remoteMultiCastEndPoint);
     } else {
         writer.Write(message);
         writer.Flush();
